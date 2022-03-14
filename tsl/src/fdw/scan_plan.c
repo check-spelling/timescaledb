5c6315dcb7a59608eae78e92f2135dbeb2508773
Josh Soref <jsoref@users.noreply.github.com>
2022-03-14 11:10:18 -0400

    spelling: aggregations
    Signed-off-by: Josh Soref <jsoref@users.noreply.github.com>

--- tsl/src/fdw/scan_plan.c
+++ tsl/src/fdw/scan_plan.c
@@ -771,7 +771,7 @@ foreign_grouping_ok(PlannerInfo *root, RelOptInfo *grouped_rel, GroupPathExtraDa
 	 * HAVING clauses and save them in remote_conds and local_conds of the
 	 * grouped rel's fpinfo.
 	 *
-	 * For partial agggregations, we never push-down the HAVING clause since
+	 * For partial aggregations, we never push-down the HAVING clause since
 	 * it either has (1) been reduced by the planner to a simple filter on the
 	 * base rel, or, in case of aggregates, the aggregates must be partials
 	 * and have therefore been pulled up into the target list (unless they're
