30227cb92c5ebbfa3cdc170c424315cc2f37313c
Josh Soref <jsoref@users.noreply.github.com>
2022-03-14 11:14:46 -0400

    spelling: update
    Signed-off-by: Josh Soref <jsoref@users.noreply.github.com>

--- test/sql/loader/timescaledb--mock-5--mock-6.sql
+++ test/sql/loader/timescaledb--mock-5--mock-6.sql
@@ -7,6 +7,6 @@ SELECT 1;
 SELECT 1;
 SELECT 1;
 SELECT 1;
---intentionally forget to updat func
+--intentionally forget to update func
 --CREATE OR REPLACE FUNCTION mock_function() RETURNS VOID
 --    AS '$libdir/timescaledb-mock-6', 'ts_mock_function' LANGUAGE C IMMUTABLE STRICT PARALLEL SAFE;
